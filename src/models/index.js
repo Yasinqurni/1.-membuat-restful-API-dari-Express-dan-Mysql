7afb0ce7c2471eceb1396f791e4034b0385622c1
yasinqurni <yalqurni@gmail.com>
2023-01-14 13:43:11 +0000

    menambah relasi table image dan item

--- src/models/index.js
+++ src/models/index.js
@@ -7,4 +7,8 @@ db.Cart = require('./cart_model')(db.sequelize, db.Sequelize)
 db.Order = require('./order_model')(db.sequelize, db.Sequelize)
 db.Images = require('./image.model')(db.sequelize, db.Sequelize)
 
+db.Item.hasMany(db.Images, {
+    foreignKey: 'item_id',
+}) 
+
 module.exports = db
